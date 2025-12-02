101f646844d6f0e9b0d2d6c9aa097596de9e7865
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-12-02 13:58:32 +0300

    Update

--- Math2001/01_Proofs_by_Calculation/04_Proving_Inequalities.lean
+++ Math2001/01_Proofs_by_Calculation/04_Proving_Inequalities.lean
@@ -28,46 +28,117 @@ example {x y : ℤ} (hx : x + 3 ≤ 2) (hy : y + 2 * x ≥ 3) : y > 3 :=
 
 -- Example 1.4.2
 -- Exercise: replace the words "sorry" with the correct Lean justification.
+-- a)
 example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 :=
   calc
-    r = (s + r + r - s) / 2 := by sorry
-    _ ≤ (3 + (s + 3) - s) / 2 := by sorry
-    _ = 3 := by sorry
+    r = ((s + r) + r - s) / 2 := by ring
+    _ ≤ (3 + r - s) / 2 := by rel [h2]
+    _ ≤ (3 + (s + 3) - s) / 2 := by rel [h1]
+    _ = 3 := by ring
+
+-- b)
+example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 :=
+  calc
+    r = (s + r) - s := by ring
+    _ ≤ 3 - s := by rel [h2]
+    _ = r + 3 - s - r := by ring
+    _ = r + 3 - (s + r) := by ring
+    _ ≤ (s + 3) + 3 - (s + r) := by rel [h1]
+    _ = 6 - r := by ring
+    _ = s + r := by sorry
+    _ ≤ 3 := by rel [h2]
 
 -- Example 1.4.3
 -- Exercise: type out the whole proof printed in the text as a Lean proof.
+
+ -- y ≤ x + 5 => (x +) y ≤ (x +) (x + 5)
 example {x y : ℝ} (h1 : y ≤ x + 5) (h2 : x ≤ -2) : x + y < 2 :=
-  sorry
+  calc
+    x + y ≤ x + (x + 5) := by rel [h1]
+    _ = 2 * x + 5 := by ring
+    _ ≤ 2 * -2 + 5 := by rel [h2]
+    _ = 1 := by ring
+    1 < 2 := by numbers
+
+-- Создать цепочку неравенств:
+-- x + y <= ... <= ... < 2
+-- В более общем виде:
+-- start (<=/</=) ... (<=/</=) ... (rel) finish
+
+-- h1: позволяет перейти от 2х переменных к одной переменной (x)
+-- h2: позволяет перейти от переменной (x) к конкретным числам
+
+--  x +  y
+--       y ≤       x + 5
+-- (x +) y ≤ (x +) x + 5
+--
+--  2 *  x + 5
+--       x     ≤       -2
+-- (2 *) x + 5 ≤ (2 *) -2 + 5
+--
+-- (2 *) -2 + 5 = 1
+--
+-- 1 < 2
 
 -- Example 1.4.4
 -- Exercise: replace the words "sorry" with the correct Lean justification.
-example {u v x y A B : ℝ} (h1 : 0 < A) (h2 : A ≤ 1) (h3 : 1 ≤ B) (h4 : x ≤ B)
-    (h5 : y ≤ B) (h6 : 0 ≤ u) (h7 : 0 ≤ v) (h8 : u < A) (h9 : v < A) :
+example {u v x y A B : ℝ}
+    (h1 : 0 < A) (h2 : A ≤ 1) (h3 : 1 ≤ B)
+    (h4 : x ≤ B) (h5 : y ≤ B)
+    (h6 : 0 ≤ u) (h7 : 0 ≤ v)
+    (h8 : u < A) (h9 : v < A) :
     u * y + v * x + u * v < 3 * A * B :=
   calc
     u * y + v * x + u * v
-      ≤ u * B + v * B + u * v := by sorry
-    _ ≤ A * B + A * B + A * v := by sorry
-    _ ≤ A * B + A * B + 1 * v := by sorry
-    _ ≤ A * B + A * B + B * v := by sorry
-    _ < A * B + A * B + B * A := by sorry
-    _ = 3 * A * B := by sorry
+      ≤ u * B + v * B + u * v := by rel [h5, h4]
+    _ ≤ A * B + A * B + A * v := by rel [h8, h9]
+    _ ≤ A * B + A * B + 1 * v := by rel [h2]
+    _ ≤ A * B + A * B + B * v := by rel [h3]
+    _ < A * B + A * B + B * A := by rel [h9]
+    _ = 3 * A * B := by ring
 
 -- Example 1.4.5
 -- Exercise: replace the words "sorry" with the correct Lean justification.
+-- a)
 example {t : ℚ} (ht : t ≥ 10) : t ^ 2 - 3 * t - 17 ≥ 5 :=
   calc
     t ^ 2 - 3 * t - 17
-      = t * t - 3 * t - 17 := by sorry
-    _ ≥ 10 * t - 3 * t - 17 := by sorry
-    _ = 7 * t - 17 := by sorry
-    _ ≥ 7 * 10 - 17 := by sorry
+      = t * t - 3 * t - 17 := by ring
+    _ ≥ 10 * t - 3 * t - 17 := by rel [ht]
+    _ = 7 * t - 17 := by ring
+    _ ≥ 7 * 10 - 17 := by rel [ht]
+    _ ≥ 5 := by numbers
+
+-- b)
+example {t : ℚ} (ht : t ≥ 10) : t ^ 2 - 3 * t - 17 ≥ 5 :=
+  calc
+    t ^ 2 - 3 * t - 17
+      = t  * t - 3 * t  - 17 := by ring
+ -- _ ≥ 10 * 10 - 3 * 10 - 17 := by rel [ht] -- error: t ≤ 10
+ -- ...
     _ ≥ 5 := by sorry
 
 -- Example 1.4.6
 -- Exercise: type out the whole proof printed in the text as a Lean proof.
+-- a)
 example {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
-  sorry
+  calc
+    n ^ 2 = n * n := by ring
+    _ ≥ 5 * 5 := by rel [hn]
+    _ > 2 * 5 + 11 := by numbers
+    -- failed to synthesize `Trans` instance Trans GT.gt LE.le ?m.31335
+    -- _ ≤ 2 * n + 11 := by sorry
+    _ > 2 * n + 11 := by sorry
+
+-- b)
+example {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
+  calc
+    n ^ 2 = n * n := by ring
+    _ ≥ 5 * n := by rel [hn]
+    _ = 2 * n + 3 * n := by ring
+    _ ≥ 2 * n + 3 * 5 := by rel [hn]
+    _ = 2 * n + 11 + 4 := by ring
+    _ > 2 * n + 11 := by extra
 
 -- Example 1.4.7
 example {m n : ℤ} (h : m ^ 2 + n ≤ 2) : n ≤ 2 :=
@@ -75,15 +146,14 @@ example {m n : ℤ} (h : m ^ 2 + n ≤ 2) : n ≤ 2 :=
     n ≤ m ^ 2 + n := by extra
     _ ≤ 2 := by rel [h]
 
-
 -- Example 1.4.8
 -- Exercise: replace the words "sorry" with the correct Lean justification.
 example {x y : ℝ} (h : x ^ 2 + y ^ 2 ≤ 1) : (x + y) ^ 2 < 3 :=
   calc
-    (x + y) ^ 2 ≤ (x + y) ^ 2 + (x - y) ^ 2 := by sorry
-    _ = 2 * (x ^ 2 + y ^ 2) := by sorry
-    _ ≤ 2 * 1 := by sorry
-    _ < 3 := by sorry
+    (x + y) ^ 2 ≤ (x + y) ^ 2 + (x - y) ^ 2 := by extra
+    _ = 2 * (x ^ 2 + y ^ 2) := by ring
+    _ ≤ 2 * 1 := by rel [h]
+    _ < 3 := by numbers
 
 -- Example 1.4.9
 -- Exercise: replace the words "sorry" with the correct Lean justification.
@@ -91,12 +161,12 @@ example {a b : ℚ} (h1 : a ≥ 0) (h2 : b ≥ 0) (h3 : a + b ≤ 8) :
     3 * a * b + a ≤ 7 * b + 72 :=
   calc
     3 * a * b + a
-      ≤ 2 * b ^ 2 + a ^ 2 + (3 * a * b + a) := by sorry
-    _ = 2 * ((a + b) * b) + (a + b) * a + a := by sorry
-    _ ≤ 2 * (8 * b) + 8 * a + a := by sorry
-    _ = 7 * b + 9 * (a + b) := by sorry
-    _ ≤ 7 * b + 9 * 8 := by sorry
-    _ = 7 * b + 72 := by sorry
+      ≤ 2 * b ^ 2 + a ^ 2 + (3 * a * b + a) := by extra
+    _ = 2 * ((a + b) * b) + (a + b) * a + a := by ring
+    _ ≤ 2 * (8 * b) + 8 * a + a := by rel [h3]
+    _ = 7 * b + 9 * (a + b) := by ring
+    _ ≤ 7 * b + 9 * 8 := by rel [h3]
+    _ = 7 * b + 72 := by ring
 
 -- Example 1.4.10
 example {a b c : ℝ} :
@@ -116,22 +186,71 @@ up in Lean. -/
 
 
 example {x y : ℤ} (h1 : x + 3 ≥ 2 * y) (h2 : 1 ≤ y) : x ≥ -1 :=
-  sorry
+  calc
+    x = x + 3 - 3 := by ring
+    _ ≥ 2 * y - 3 := by rel [h1]
+    _ ≥ 2 * 1 - 3 := by rel [h2]
+    _ ≥ -1 := by numbers
 
-example {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 :=
-  sorry
+example {a b : ℚ} (h1 : a ≥ 3) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 :=
+  calc
+    a + b = (2 * a + 2 * b) / 2 := by ring
+    _ = (a + (a + 2 * b)) / 2 := by ring
+    _ ≥ (a + 4) / 2 := by rel [h2]
+    _ ≥ (3 + 4) / 2 := by rel [h1]
+    _ = 7 / 2 := by ring
+    _ ≥ 3 := by numbers
 
 example {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 :=
-  sorry
+  calc
+    x ^ 3 - 8 * x ^ 2 + 2 * x
+      = x * x * x - 8 * x * x + 2 * x := by ring
+    _ ≥ 9 * x * x - 8 * x * x + 2 * 9 := by rel [hx]
+    _ = x * x + 18 := by ring
+    _ ≥ 9 * 9 + 18 := by rel [hx]
+    _ ≥ 3 := by numbers
+
+-- (n + 1) ^ 4 − (n − 1) ^ 4 = 8 * n ^ 3 + 8 * n.
 
 example {n : ℤ} (hn : n ≥ 10) : n ^ 4 - 2 * n ^ 2 > 3 * n ^ 3 :=
-  sorry
+  calc
+    n ^ 4 - 2 * n ^ 2
+      = n ^ 2 * n ^ 2 - 2 * n ^ 2 := by ring
+    _ = n ^ 2 * (n * n - 2) := by ring
+    _ ≥ n ^ 2 * (10 * n - 2) := by rel [hn]
+    _ > n ^ 2 * (3 * n) := by sorry
+    _ = n ^ 3 + 2 * n ^ 3 := by ring
+    _ = 3 * n ^ 3 := by ring
+
+-- x = n ^ 2
+-- x ^ 2 - 2 * 1 * x + 1 ^ 2 - 1
+-- (x - 1) ^ 2 - 1
+-- (n ^ 2 - 1) ^ 2 - 1
 
 example {n : ℤ} (h1 : n ≥ 5) : n ^ 2 - 2 * n + 3 > 14 :=
-  sorry
+  calc
+    n ^ 2 - 2 * n + 3 = (n - 1) ^ 2 + 2 := by ring
+    _ = 2 + (n - 1) ^ 2 := by rw [add_comm]
+    _ ≥ 2 + (5 - 1) ^ 2 := by rel [h1]
+    _ = 18 := by numbers
+    _ > 14 := by numbers
 
 example {x : ℚ} : x ^ 2 - 2 * x ≥ -1 :=
-  sorry
+  calc
+    x ^ 2 - 2 * x = (x - 1) ^ 2 - 1 := by ring
+    _ ≥ -1 := by extra
+
+-- a ^ 2 + b ^ 2 + 2 * a * b
+-- (a + b) ^ 2
+
+-- 2 * a * b + 2 * a * b
+-- 4 * a * b
+
+-- Показать, что левая + что-то >= правая
+-- 2ab + C ≥ 2ab (by extra)
 
 example (a b : ℝ) : a ^ 2 + b ^ 2 ≥ 2 * a * b :=
-  sorry
+  calc
+    a ^ 2 + b ^ 2 = 2 * a * b + (a ^ 2 + b ^ 2 - 2 * a * b) := by ring
+    _ = 2 * a * b + (a - b) ^ 2 := by ring
+    _ ≥ 2 * a * b := by extra
--- Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
+++ Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
@@ -6,9 +6,11 @@ math2001_init
 
 /-! # Section 1.5: A shortcut -/
 
-example {x : ℤ} (h1 : x + 4 = 2) : x = -2 := by addarith [h1]
+example {x : ℤ} (h1 : x + 4 = 2) : x = -2 :=
+  by addarith [h1]
 
-example {a b : ℤ} (ha : a - 2 * b = 1) : a = 2 * b + 1 := by addarith [ha]
+example {a b : ℤ} (ha : a - 2 * b = 1) : a = 2 * b + 1 :=
+  by addarith [ha]
 
 example {x y : ℚ} (hx : x = 2) (hy : y ^ 2 = -7) : x + y ^ 2 = -5 :=
   calc
